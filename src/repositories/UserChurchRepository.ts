54363f8430cadfb1b8275b94d2b2bfd510ab94d9
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-06-24 21:53:27 +0530

    create base UserChurchRepository file

---
+++ src/repositories/UserChurchRepository.ts
@@ -0,0 +1,28 @@
+import { DB } from '../apiBase/db';
+import { UserChurch } from '../models';
+import { UniqueIdHelper } from '../helpers';
+
+export class UserChurchRepository {
+
+    public save(userChurch: UserChurch) {
+        return userChurch.id ? this.update(userChurch) : this.create(userChurch);
+    }
+
+    private async create(userChurch: UserChurch) {
+        userChurch.id = UniqueIdHelper.shortId();
+        const { id, userId, churchId, personId } = userChurch;
+        const sql = "INSERT INTO userChurches (id, userId, churchId, personId) values (?, ?, ?, ?)";
+        const params = [id, userId, churchId, personId];
+        await DB.query(sql, params);
+        return userChurch;
+    }
+
+    private async update(userChurch: UserChurch) {
+        const { id, userId, churchId, personId } = userChurch;
+        const sql = "UPDATE userChurches SET userId=?, churchId=?, personId=? WHERE id=?;";
+        const params = [userId, churchId, personId, id];
+        await DB.query(sql, params);
+        return userChurch;
+    }
+
+}
